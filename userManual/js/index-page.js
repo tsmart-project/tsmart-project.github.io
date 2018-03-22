868fd668711774edfd3d96bfd1f961af036312a4
tsmart-project <tsmart.project@gmail.com>
2018-03-22 15:00:08 +0800

    Updated user manual

--- userManual/js/index-page.js
+++
@@ -1,4 +0,0 @@
-/*! HelpSmith Web Help System 2.1
-* http://www.helpsmith.com
-* Copyright (c) 2007-2014 Divcom Software */
-$(document).ready(function(){var b=$("frame[name=topic]"),a=getTopicFromQuery(b.attr("src"));if(!1===a)a="unknown.htm";else{var c=getQueryAnchor();""!=c&&(a+="#"+c)}naviReady(function(){b.attr("src",a)})});
--- userManual/js/topic.js
+++
@@ -1,4 +0,0 @@
-/*! HelpSmith Web Help System 2.1
-* http://www.helpsmith.com
-* Copyright (c) 2007-2014 Divcom Software */
-$(document).ready(function(){highlightSearchWords();syncToc(topicContext)});function syncToc(b,d){if(0==top.frames.length){if("0"!==getParamValue("frames",get_getQuery())){var a="../"+getWHSettings("indexFile","index.htm")+"?context="+b,c=getQueryAnchor();""!=c&&(a+="#"+c);window.location=a}}else naviReady(function(){top.navi.setCurrentTopic(b)})};
